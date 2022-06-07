91ae73c5771f4a87867209985a8b4a3d38585f43
Ricky Thakar <rickythakar@gmail.com>
2022-06-07 14:13:25 -0400

    refactored code for random recipe js page

--- client/src/pages/RandomRecipe.js
+++ client/src/pages/RandomRecipe.js
@@ -1,20 +1,11 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
 import Button from 'react-bootstrap/Button';
 import Test from "../cookie-tin.jpg";
 import { Link } from "react-router-dom";
-=======
-// import button from 'react-bootstrap/button';
->>>>>>> 88d97846cca72420992ab4d32bebc64849d0d96e
 
 function RandomRecipe(props) {
     return (
         <div className='container'>
-<<<<<<< HEAD
-        <Link to="/random">
-          <Button>Not Interested</Button>
-        </Link>
-
             <div className = "px-4 pb-4">
         <div className="ayo max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
@@ -39,12 +30,17 @@ function RandomRecipe(props) {
             <div className = "flex justify-between">
             {/* Buttons need to have an OnClick for adding to you new CookBook */}
             <button href="#" className="px-4 inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Save Food
+                read more
                 <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
+{/* 
+            <button href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                really love it
+                <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </button> */}
 
             <button href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                New Result
+                save 
                 <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
 
@@ -52,28 +48,14 @@ function RandomRecipe(props) {
       </div>
     </div>
     <Link to="/random">
+          <Button>Not Interested</Button>
+        </Link>
+    <Link to="/random">
           <Button>I like it, Picasso</Button>
         </Link>
             </div>
-=======
-            <button>
-                button1
-            </button>
-            <div>
-                Container For Recipe Card
-                
-            </div>
-            <button>
-                button2
-            </button>
->>>>>>> 88d97846cca72420992ab4d32bebc64849d0d96e
         </div>
-
-
     )
-
 }
-
-
 
 export default RandomRecipe;
