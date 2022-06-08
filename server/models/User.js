86241f5d6d13b652fabfe2bcd2a6b306a4d61042
Rene Ortega <reneortega2468@gmail.com>
2022-06-08 18:43:24 -0400

    working signin

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,16 @@
+const mongoose = require('mongoose');
+
 const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
- // will need recipe schema
+const Order = require('./Order');
 
 const userSchema = new Schema({
-  userName: {
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
     type: String,
     required: true,
     trim: true
@@ -11,18 +18,17 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Must use a valid email address'],
+    unique: true
   },
   password: {
     type: String,
     required: true,
     minlength: 5
   },
-  recipes: [Recipe.schema]
+  orders: [Order.schema]
 });
 
-//  create password
+// set up pre-save middleware to create password
 userSchema.pre('save', async function(next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
@@ -32,11 +38,11 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
-// password check
+// compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function(password) {
   return await bcrypt.compare(password, this.password);
 };
-  // assign variable for export
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
