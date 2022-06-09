d9c4086d51d4b034d9ea15789093096af2d39787
Ricky Thakar <rickythakar@gmail.com>
2022-06-09 12:46:34 -0400

    added recipe and cuisine page to project

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -3,7 +3,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/client';
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
-import CartItem from '../CartItem';
+import CartItem from '../../pages/CartItem';
 import Auth from '../../utils/auth';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
