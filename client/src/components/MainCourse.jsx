3113ca89404f2c7ec62fb6f84c2b67174f264248
fusionneo <fusion.neoseeker@gmail.com>
2022-06-09 21:43:16 -0400

    Rewrote the random page functionality.

--- client/src/components/MainCourse.jsx
+++ client/src/components/MainCourse.jsx
@@ -4,44 +4,38 @@ import { Splide, SplideSlide } from '@splidejs/react-splide'
 import "@splidejs/splide/dist/css/splide.min.css";
 import { Link } from "react-router-dom";
 
-
-function Popular() {
-  const [popular, setPopular] = useState([]);
+function MainCourse() {
+  const [mainCourse, setMainCourse] = useState([]);
 
   useEffect(() => {
-    getPopular();
+    getMainCourse();
   }, [])
 
-  const getPopular = async () => {
+  const getMainCourse = async () => {
 
-    const check = localStorage.getItem('popular')
-    if(check) {
-      setPopular(JSON.parse(check));
-    }
-    else {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=main+course`)
     const data = await api.json();
-    localStorage.setItem('popular', JSON.stringify(data.recipes));
-    setPopular(data.recipes)
+    setMainCourse(data.recipes)
     console.log(data.recipes)
-  }}
+  }
+
   return (
     <div>
       <Wrapper>
-        <h3>Popular Picks</h3>
+        <h3>Magnificent Main Courses</h3>
         <Splide options={{
-          perPage: 4,
+          perPage: 3,
           arrows: false,
           pagination: false,
           drag: 'free',
           gap: '5rem'
 
         }}>
-          {popular.map((recipe) => {
+          {mainCourse.map((recipe) => {
             return (
               <SplideSlide key={recipe.id}>
                 <Card>
-                  <Link to={"/recipe/" + recipe.id}>
+                <Link to={"/recipe/" + recipe.id}>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
                   <Gradient />
@@ -101,5 +95,6 @@ const Gradient = styled.div`
   width: 100%;
   height: 100%;
   background: linear-gradient(rgba(0,0,0,0)), rgba(0,0,0,0.5);
-`
-export default Popular
+  `
+
+export default MainCourse
